214012a90748527f4beb9cf0bc333939371c06e7
Boris S <mr_sagan@icloud.com>
2021-02-14 00:52:22 +0200

    Remove commented code

--- food-places-ios/Application Logic/Models/Place.swift
+++ food-places-ios/Application Logic/Models/Place.swift
@@ -26,20 +26,3 @@ extension Place {
       type: entity.type)
   }
 }
-
-
-//  required convenience init(from decoder: Decoder) throws {
-//    guard let context = decoder.userInfo[CodingUserInfoKey.managedObjectContext] as? NSManagedObjectContext else {
-//      throw FPError.missingManagedObjectContext
-//    }
-//
-//    self.init(context: context)
-//
-//    let container = try decoder.container(keyedBy: CodingKeys.self)
-//    self.address = try container.decode(String?.self, forKey: .address)
-//    self.name = try container.decode(String?.self, forKey: .name)
-//    self.phoneNumber = try container.decode(String?.self, forKey: .phoneNumber)
-//    self.longName = try container.decode(String?.self, forKey: .longName)
-//    self.url = try container.decode(String?.self, forKey: .url)
-//    self.type = try container.decode(String?.self, forKey: .type)
-//  }
